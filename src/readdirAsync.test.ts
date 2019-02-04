f471eed020a26dca5789703969268e71856fb5da
Juan Manuel Cañabate <juanmanuel@patagonian.it>
2019-02-04 16:44:49 -0300

    update snapshoots to reflect new files

--- src/readdirAsync.test.ts
+++ src/readdirAsync.test.ts
@@ -1,73 +1,30 @@
 import readdirAsync from './readdirAsync'
 
 test('the project has the expected files', async () => {
-  const dirents = await readdirAsync(process.cwd())
+  const dirents = (await readdirAsync(process.cwd()))
+    .map(dirent => dirent.name)
+    .sort()
+
   expect(dirents).toMatchInlineSnapshot(`
 Array [
-  Dirent {
-    "name": "jest.config.js",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": ".git",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": ".prettierignore",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": ".prettierrc",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": ".tmp",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": ".vscode",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": "README.md",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": "dist",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": ".gitignore",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": "node_modules",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": "package.json",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": "scripts",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": "src",
-    Symbol(type): 2,
-  },
-  Dirent {
-    "name": "tsconfig.json",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": "tslint.json",
-    Symbol(type): 1,
-  },
-  Dirent {
-    "name": "yarn.lock",
-    Symbol(type): 1,
-  },
+  ".babelrc",
+  ".git",
+  ".gitignore",
+  ".prettierignore",
+  ".prettierrc",
+  ".tmp",
+  ".vscode",
+  "CHANGELOG.md",
+  "README.md",
+  "dist",
+  "jest.config.js",
+  "node_modules",
+  "package.json",
+  "scripts",
+  "src",
+  "tsconfig.json",
+  "tslint.json",
+  "yarn.lock",
 ]
 `)
 })
